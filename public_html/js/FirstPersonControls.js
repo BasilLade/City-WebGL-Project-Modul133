007a27448c9f45277750c52fddec0d18f5ae8b19
LordKnoppers <flodderone@gmail.com>
2015-04-10 23:33:06 +0200

    variable keyboardMovement to disable Movent with w,a,s,d and arrow keys

--- public_html/js/FirstPersonControls.js
+++ public_html/js/FirstPersonControls.js
@@ -19,6 +19,7 @@ THREE.FirstPersonControls = function (object, domElement) {
     // this.invertVertical = false;
 
     this.activeLook = true;
+    this.keyboardMovement = true;
 
     this.heightSpeed = false;
     this.heightCoef = 1.0;
@@ -117,60 +118,64 @@ THREE.FirstPersonControls = function (object, domElement) {
 
         //event.preventDefault();
 
-        switch (event.keyCode) {
-            case 38: /*up*/
-            case 87: /*W*/
-                this.moveForward = true;
-                break;
-            case 37: /*left*/
-            case 65: /*A*/
-                this.moveLeft = true;
-                break;
-            case 40: /*down*/
-            case 83: /*S*/
-                this.moveBackward = true;
-                break;
-            case 39: /*right*/
-            case 68: /*D*/
-                this.moveRight = true;
-                break;
-            case 82: /*R*/
-                this.moveUp = true;
-                break;
-            case 70: /*F*/
-                this.moveDown = true;
-                break;
-            case 81: /*Q*/
-                this.freeze = !this.freeze;
-                break;
+        if (this.keyboardMovement) {
+            switch (event.keyCode) {
+                case 38: /*up*/
+                case 87: /*W*/
+                    this.moveForward = true;
+                    break;
+                case 37: /*left*/
+                case 65: /*A*/
+                    this.moveLeft = true;
+                    break;
+                case 40: /*down*/
+                case 83: /*S*/
+                    this.moveBackward = true;
+                    break;
+                case 39: /*right*/
+                case 68: /*D*/
+                    this.moveRight = true;
+                    break;
+                case 82: /*R*/
+                    this.moveUp = true;
+                    break;
+                case 70: /*F*/
+                    this.moveDown = true;
+                    break;
+                case 81: /*Q*/
+                    this.freeze = !this.freeze;
+                    break;
+            }
         }
     };
 
     this.onKeyUp = function (event) {
 
-        switch (event.keyCode) {
-            case 38: /*up*/
-            case 87: /*W*/
-                this.moveForward = false;
-                break;
-            case 37: /*left*/
-            case 65: /*A*/
-                this.moveLeft = false;
-                break;
-            case 40: /*down*/
-            case 83: /*S*/
-                this.moveBackward = false;
-                break;
-            case 39: /*right*/
-            case 68: /*D*/
-                this.moveRight = false;
-                break;
-            case 82: /*R*/
-                this.moveUp = false;
-                break;
-            case 70: /*F*/
-                this.moveDown = false;
-                break;
+        if (this.keyboardMovement) {
+            switch (event.keyCode) {
+                case 38: /*up*/
+                case 87: /*W*/
+                    this.moveForward = false;
+                    break;
+                case 37: /*left*/
+                case 65: /*A*/
+                    this.moveLeft = false;
+                    break;
+                case 40: /*down*/
+                case 83: /*S*/
+                    this.moveBackward = false;
+                    break;
+                case 39: /*right*/
+                case 68: /*D*/
+                    this.moveRight = false;
+                    break;
+                case 82: /*R*/
+                    this.moveUp = false;
+                    break;
+                case 70: /*F*/
+                    this.moveDown = false;
+                    break;
+            }
         }
     };
 
